f3b93626592c858e8ceb515dc75e11cf99a81308
Zhaojuntao-king <2401212755@stu.pku.edu.cn>
2026-04-08 10:25:13 +0800

    Update once-ui.config.ts

--- src/resources/once-ui.config.ts
+++ src/resources/once-ui.config.ts
@@ -19,8 +19,6 @@ const baseURL: string = "https://demo.magic-portfolio.com";
 const routes: RoutesConfig = {
   "/": true,
   "/about": true,
-  "/work": true,
-  "/blog": true,
   "/gallery": true,
 };
 
